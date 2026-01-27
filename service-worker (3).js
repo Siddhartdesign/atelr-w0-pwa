25cb0b1f21905e9e614ed658e4e507d77ab85182
SidArtDesignLab <mmaheshverma1@gmail.com>
2026-01-27 11:12:12 +0530

    Add files via upload

---
+++ service-worker (3).js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "atelier-tools-cache-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
